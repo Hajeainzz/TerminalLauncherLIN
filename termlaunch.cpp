7d5e9ba58146146b755e6d3a7ce037f9c10b9f1a
Hajeainzz <114838@lmusd.org>
2026-04-17 16:12:22 -0700

    Finalized termlaunch.cpp

--- termlaunch.cpp
+++ termlaunch.cpp
@@ -1,3 +1,16 @@
+//  _____                   _             _                                
+// |_   _|__ _ __ _ __ ___ (_)_ __   __ _| |                               
+//   | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | |                               
+//   | |  __/ |  | | | | | | | | | | (_| | |                               
+//  _|_|\___|_|  |_| |_| |_|_|_|_|_|\__,_|_|      _     _                  
+// | |    __ _ _   _ _ __   ___| |__   ___ _ __  | |   (_)_ __  _   ___  __
+// | |   / _` | | | | '_ \ / __| '_ \ / _ \ '__| | |   | | '_ \| | | \ \/ /
+// | |__| (_| | |_| | | | | (__| | | |  __/ |    | |___| | | | | |_| |>  < 
+// |_____\__,_|\__,_|_| |_|\___|_| |_|\___|_|    |_____|_|_| |_|\__,_/_/\_\
+
+// This is a free linux launcher thats easily customizable and amazing to use! Feel free to fork this and edit the code however you want!
+
+
 #include <iostream>
 #include <string>
 #include <thread>
@@ -31,9 +44,15 @@ int main() {
     std::cout << "=        Launcher Menu        =\n";
     std::cout << "===============================\n";
     std::cout << "Welcome! Please input what you would like to launch.\n";
+    //   ____          _                  _           __  __      
+    //  / ___|   _ ___| |_ ___  _ __ ___ (_)_______  |  \/  | ___ 
+    // | |  | | | / __| __/ _ \| '_ ` _ \| |_  / _ \ | |\/| |/ _ \
+    // | |__| |_| \__ \ || (_) | | | | | | |/ /  __/ | |  | |  __/
+    //  \____\__,_|___/\__\___/|_| |_| |_|_/___\___| |_|  |_|\___|
+    
     // Options! CHANGE THiS PART IF YOU JUST CLAImED THE REPO!! Add on if you have more.
-    std::cout << "1. Discord";
-    std::cout << "2. Chrome"
+    std::cout << "1. Discord\n";
+    std::cout << "2. firefox\n";
 
 
     // This determines the launcher number.
@@ -43,7 +62,19 @@ int main() {
     // Main if Function
 
     if (LauncherNumber == 1) {
-        system("discord")
+        system("discord");
+        std::cout << "Launching discord";
     }
-
+    if (LauncherNumber == 2) {
+        system("firefox");
+        std::cout << "Launching Firefox.";
+    } 
+    if (LauncherNumber == 3) {
+        std::cout << "Add on more and customize the code to have more!";
+    }
 }
+
+
+// fans = alot. 
+// graphics = real.
+// good game = true.
